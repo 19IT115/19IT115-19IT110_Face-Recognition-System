d0a5405c675fc243500059086ba9d9700841a3d0
19IT110 <68862319+19IT110@users.noreply.github.com>
2021-03-17 09:08:53 -0800

    Add files via upload

---
+++ Eye_Detection.py
@@ -0,0 +1,44 @@
+import cv2
+
+cascade_classifier = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+eyeCascade = cv2.CascadeClassifier('haarcascades/haarcascade_eye.xml')
+cascPath = "haarcascade_frontalface_default.xml"
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)  # set Width
+cap.set(4, 480)  # set Height
+
+while True:
+    ret, img = cap.read()
+    #img = cv2.flip(img, -1)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = cascade_classifier.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = img[y:y + h, x:x + w]
+
+        eyes = eyeCascade.detectMultiScale(
+            roi_gray,
+            scaleFactor=1.5,
+            minNeighbors=10,
+            minSize=(5, 5),
+        )
+
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+        cv2.imshow('video', img)
+
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:  # press 'ESC' to quit
+        break
+
+cap.release()
+cv2.destroyAllWindows()
